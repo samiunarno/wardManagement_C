9785b9ea5a7c2560d4c391842f310cc6f5360ea8
samiunarno <samiunarno.iub@gmail.com>
2026-04-03 11:08:15 +0800

    Clean repo: removed all source code and binaries

--- services/report_service.h
+++
@@ -1,9 +0,0 @@
-#ifndef REPORT_SERVICE_H
-#define REPORT_SERVICE_H
-
-#include "bed_management.h"
-
-void generate_and_display_reports(BedManagementSystem* system);
-void export_all_reports(BedManagementSystem* system);
-
-#endif
